1afab3573a663f8801d922c5540fc16707f00b27
Yuxuan Shi <1051268839@qq.com>
2023-12-04 12:27:21 -0500

    Update

--- src/hooks/useConfig.js
+++ src/hooks/useConfig.js
@@ -4,7 +4,7 @@
  * development context / deployed version.
  */
 
-// export const ENDPOINT_URL =
-//   "https://unitask-backend-impl-72c59f313288.herokuapp.com/";
+export const ENDPOINT_URL =
+  "https://unitask-backend-impl-72c59f313288.herokuapp.com/";
 
-export const ENDPOINT_URL = "http://localhost:8080/";
+// export const ENDPOINT_URL = "http://localhost:8080/";
